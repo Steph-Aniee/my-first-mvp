6c143679f3230d5a28a0773a104d396ba2c4da22
Steph-Aniee <119601242+Steph-Aniee@users.noreply.github.com>
2023-05-22 20:32:18 +0200

    Debugged routes

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, "public"))); //not using public folder
 
-app.use("/api", menuRouter);
-app.use("/api/choc_of_month", chocoMoRouter);
+app.use("/", menuRouter);
+app.use("/", chocoMoRouter);
 
 module.exports = app;
--- routes/menu_items.js
+++ routes/menu_items.js
@@ -2,8 +2,13 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.send({ title: "Express" });
+router.get("/api", async function (req, res, next) {
+  try {
+    console.log("hi");
+    res.send("Express");
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 module.exports = router;
