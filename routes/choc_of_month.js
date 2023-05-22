d0cd1a5f1b2658713238afd687228930b101a84d
Steph-Aniee <119601242+Steph-Aniee@users.noreply.github.com>
2023-05-22 21:31:56 +0200

    Added comments for endpoints I need

--- routes/choc_of_month.js
+++ routes/choc_of_month.js
@@ -1,9 +1,15 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
+/* GET choc of the month. */
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
+
+/* POST choc of the month. */
+
+/* DELETE choc of the month. */
+
+/* PUT choc of the month. */
 
 module.exports = router;
--- routes/menu_items.js
+++ routes/menu_items.js
@@ -1,14 +1,21 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
+/* GET menu items. */
 router.get("/", async function (req, res, next) {
   try {
-    console.log("hi");
     res.send("Express");
   } catch (error) {
     res.status(500).send(error);
   }
 });
+
+/* POST menu items. */
+
+/* DELETE menu items. */
+
+/* PUT menu items. */
+
+/* PUT only DATE of menu items. */
 
 module.exports = router;
