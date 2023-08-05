ca69314fc288be287c55ea58bd731de408683a39
Steph-Aniee <119601242+Steph-Aniee@users.noreply.github.com>
2023-08-05 17:36:30 +0200

    deploy prep

--- model/helper.js
+++ model/helper.js
@@ -4,27 +4,29 @@ const mysql = require("mysql");
 module.exports = async function db(query) {
   const results = {
     data: [],
-    error: null
+    error: null,
   };
   let promise = await new Promise((resolve, reject) => {
     const DB_HOST = process.env.DB_HOST;
     const DB_USER = process.env.DB_USER;
     const DB_PASS = process.env.DB_PASS;
     const DB_NAME = process.env.DB_NAME;
+    const DB_PORT = process.env.DB_PORT;
 
     const con = mysql.createConnection({
       host: DB_HOST || "127.0.0.1",
+      port: DB_PORT,
       user: DB_USER || "root",
       password: DB_PASS,
       database: DB_NAME || "database",
-      multipleStatements: true
+      multipleStatements: true,
     });
 
-    con.connect(function(err) {
+    con.connect(function (err) {
       if (err) throw err;
       console.log("Connected!");
 
-      con.query(query, function(err, result) {
+      con.query(query, function (err, result) {
         if (err) {
           results.error = err;
           console.log(err);
@@ -47,7 +49,7 @@ module.exports = async function db(query) {
           // results.data.push(result);
         } else if (result[0].constructor.name == "RowDataPacket") {
           // push each row (RowDataPacket) to data
-          result.forEach(row => results.data.push(row));
+          result.forEach((row) => results.data.push(row));
         } else if (result[0].constructor.name == "OkPacket") {
           // push the first item in result list to data (this accounts for situations
           // such as when the query ends with SELECT LAST_INSERT_ID() and returns an insertId)
